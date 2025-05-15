967450006afe036bca6953f00c9ec9e4b3f524d1
Satya-Prakash-Dwivedi <dwivediprakashsatya36@gmail.com>
2025-05-15 19:14:06 +0530

    resolved the dealyed response in PostForm

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,12 +1,13 @@
 import { Button, Form, Input, message } from 'antd';
 import { useSetRecoilState } from 'recoil';
 import { postsState } from '../recoil/atoms';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { api } from '../api/posts';
 
 export default function PostForm() {
   const setPosts = useSetRecoilState(postsState);
   const [form] = Form.useForm();
+  const queryClient = useQueryClient();
 
   const mutation = useMutation({
     mutationFn: (post) =>
@@ -15,6 +16,7 @@ export default function PostForm() {
       setPosts((prev) => [res.data, ...prev]);
       message.success('Post created!');
       form.resetFields();
+      queryClient.invalidateQueries(['posts']);
     },
   });
 
